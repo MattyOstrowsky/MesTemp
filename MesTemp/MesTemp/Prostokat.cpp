b9d32935b0cf5ac3f367e1c216af3ffae2534602
Krzysztof Mielczarek <216705@edu.p.lodz.pl>
2019-12-02 21:16:29 +0100

    wypełnianie lokalnych macierzy dla prostokątów

--- MesTemp/MesTemp/Prostokat.cpp
+++ MesTemp/MesTemp/Prostokat.cpp
@@ -13,3 +13,20 @@ void Prostokat::ust_temp(float t1, float t2, float t3, float t4)
 	T3 = t3;
 	T4 = t4;
 }
+
+void Prostokat::lokalna(float lamx, float lamy, float(&macierz)[][4], float (&P)[4], float wys, float szer)
+{
+	int mx[4][4] = { {2,-2,-1,1},{-2,2,1,-1},{-1,1,2,-2},{1,-1,-2,2} };
+	int my[4][4] = { {2,1,-1,-2},{1,2,-2,-1},{-1,-2,2,1},{-2,-1,1,2} };
+	float pomx = (lamx * wys) / (12 * szer);
+	float pomy = (lamy * wys) / (12 * szer);
+	float pomp = Q * szer * wys / 4;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			macierz[i][j] = pomx * mx[i][j] + pomy * my[i][j];
+		}
+		P[i] = pomp;
+	}
+}
