b084335162a678b4b1eca708d9e0fb5f2b6a6370
Krzysztof Mielczarek <216705@edu.p.lodz.pl>
2020-01-07 16:38:36 +0100

    Poprawa klasy licz: 1) klasa Prostokąt wywalona 2) dodane funkcje ktory_el i ktory_obszar

--- MesTemp/MesTemp/licz.cpp
+++ MesTemp/MesTemp/licz.cpp
@@ -2,13 +2,32 @@
 #include "licz.h"
 
 
-// funkcja zarz¹dzaj¹ca tworzeniem macierzy globalnej i rozwi¹zywaniem uk³adu równañ
-void licz::rozw(std::vector<Prostokat> dane, std::vector<float>& wyniki, Siatka S, std::vector<Input> obszary)
+void licz::lokalna(float lamx, float lamy, float(&macierz)[16], float(&P)[4], float wys, float szer, float Q)
 {
+	int mx[4][4] = { {2,-2,-1,1},{-2,2,1,-1},{-1,1,2,-2},{1,-1,-2,2} };
+	int my[4][4] = { {2,1,-1,-2},{1,2,-2,-1},{-1,-2,2,1},{-2,-1,1,2} };
+	float pomx = (lamx * wys) / (12 * szer);
+	float pomy = (lamy * wys) / (12 * szer);
+	float pomp = Q * szer * wys / 4;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			macierz[i + j * 4] = pomx * mx[i][j] + pomy * my[i][j];
+		}
+		P[i] = pomp;
+	}
+}
+
+// funkcja zarz¹dzaj¹ca tworzeniem macierzy globalnej i rozwi¹zywaniem uk³adu równañ
+void licz::rozw(std::vector<float>& wyniki, Siatka S, std::vector<Input> obszary)
+{
+	licz R;
+	//vector<flloat> wyniki (ile_wezlow, 0);
 	//lx i ly powinny byæ sk¹dœ brane- siatka? prostok¹t? input???
 	int ilex = S.kord_x.size();
 	int iley = S.kord_y.size();
-	int ile = dane.size();
+	int ile = (ilex - 1) * (iley - 1);
 	int nx, ny;		//iloœæ elementów w szeregu i kolumnie siatki
 	std::vector<float>x1 = {};
 	std::vector<float>x2 = {};
@@ -21,42 +40,111 @@ void licz::rozw(std::vector<Prostokat> dane, std::vector<float>& wyniki, Siatka 
 	float pom[4][4];
 	float lx, ly;	//wspó³czynniki przewodnoœci termicznej
 	float dx, dy;		//szerokoœæ i wysokoœæ elementu
+	int o = 0;
+	bool B = false;
 	//jakaœ zewnêtrzna pêtla do latania po ró¿nych obszarach?
 
-	for (int i = 0; i < ile; i++)
+	for (int x = 0; x < ilex-1; x++)
 	{
-		ny = i / ilex;
-		nx = i % ilex;
-		dx = S.kord_x[nx + 1] - S.kord_x[nx];
-		dy = S.kord_y[ny + 1] - S.kord_y[ny];
-		lx = obszary[dane[i].obszar].przewodnosc_x;
-		ly = obszary[dane[i].obszar].przewodnosc_y;
-		dane[i].lokalna(lx, ly, lok, Q, dx, dy);
-		for (int k = 0; k < 4; k++)
+		for (int y = 0; y < iley-1; y++)
 		{
-			for (int l = 0; l < 4; l++)
+			dx = S.kord_x[x + 1] - S.kord_x[x];
+			dy = S.kord_y[y + 1] - S.kord_y[y];
+			R.ktory_obszar((S.kord_x[x] + 0.5 * dx), (S.kord_y[y] + 0.5 * dy), o, S, B, obszary);
+			lx = obszary[o].przewodnosc_x;
+			ly = obszary[o].przewodnosc_y;
+			R.lokalna(lx, ly, lok, Q, dx, dy, obszary[o].moc_zrodla);
+			for (int k = 0; k < 4; k++)
 			{
-				pom[k][l] = lok[k + 4 * l];
+				for (int l = 0; l < 4; l++)
+				{
+					pom[k][l] = lok[k + 4 * l];
+				}
 			}
+			wsp[0] = y*ilex + x;
+			wsp[1] = y*ilex  + x + 1;
+			wsp[2] = (y+1) * ilex + x;
+			wsp[3] = (y + 1) * ilex + x + 1;
+			M.do_globalnej(M, pom, Q, Qg, wsp);
 		}
-		wsp[0] = dane[i].n1;
-		wsp[1] = dane[i].n1+1;
-		wsp[2] = dane[i].n1+ilex;
-		wsp[3] = dane[i].n1+ilex+1;
-		M.do_globalnej(M, pom, Q, Qg, wsp);
 	}
 	M.licz(M, Qg, wyniki);
 }
 
 
 // funkcja podaj¹ca temperaturê w danym punkcie. ost-numer elementu w ostatnio wyszukiwanym punkcie
-float licz::temp(float x, float y, int& ost, std::vector<float> T, std::vector<Prostokat>E, Siatka S)
+float licz::temp(float x, float y, int& ost, std::vector<float> T, Siatka S)
 {
+	licz L;
 	int n1;
 	float nx, ny, kx, ky, lokx, loky, w;
 	bool czy = true;	//czy kontynuowaæ poszukiwania elementu
+	bool X = false;		//zmienna awaryjna, bada prawid³owy przebieg pêtli
 	//zamiast zwyk³ego fora pêtla zaczyna poszukiwania od sprawdzenia ostatnio u¿ytego elementu. Nie doda³em obs³ugi b³êdów, wiêc
 	//JEŒLI PROGRAM WIESZA SIÊ NA TEJ PÊTLI TO ZNACZY, ¯E WSKAZANY PUNKT JEST POZA GRANICAMI SIATKI
+	L.ktory_el (x, y, ost, S, X);
+	//Po powy¿szej pêtli wiadomo ju¿, w którym elemencie znajduje siê punkt
+	ny = ost / S.kord_y.size();
+	nx = ost % S.kord_x.size();
+	if (X) return -300;
+	else
+	{
+		kx = (S.kord_x[nx + 1] - S.kord_x[nx]) * 0.5;
+		ky = (S.kord_y[ny + 1] - S.kord_y[ny]) * 0.5;		//wspó³czynniki proporcjonalnoœci
+		lokx = (x - S.kord_x[nx]) / kx - 1;
+		loky = (y - S.kord_y[ny]) / ky - 1;		//lokalne wspó³zêdne (-1;1)
+		n1 = nx + ny * S.kord_x.size();			//numer lewego dolnego wêz³a
+		w = T[n1] * (1 - lokx) * (1 - loky);
+		w += T[n1 + 1] * (1 + lokx) * (1 - loky);
+		w += T[n1 + S.kord_x.size()] * (1 - lokx) * (1 + loky);
+		w += T[n1 + S.kord_x.size() + 1] * (1 + lokx) * (1 + loky);
+		w *= 0.25;
+		return w;
+	}
+}
+
+// funkcja zwracaj¹ca numer obszaru pod wskazanymi wspó³rzêdnymi
+void licz:: ktory_obszar(float x, float y, int& ost, Siatka S, bool& X, std::vector <Input> obszary)
+{
+	bool czy = true;	//czy kontynuowaæ poszukiwania elementu
+	X = false;		//zmienna awaryjna, bada prawid³owy przebieg pêtli
+	//zamiast zwyk³ego fora pêtla zaczyna poszukiwania od sprawdzenia ostatnio u¿ytego elementu.
+	//JEŒLI PUNKT NIE NALE¯Y DY ¯ADNEGO OBSZARU, TO X =  TRUE 
+	while (czy)
+	{
+		if (x >= obszary[ost].x1 && x <= obszary[ost].x4 && y >= obszary[ost].y1 && obszary[ost].y4)
+		{
+			czy = false;
+		}
+		else
+		{
+			if (ost == obszary.size()-1)
+			{
+				ost = 0;
+				if (X)
+				{
+					czy = false;
+				}
+				else
+				{
+					X = true;
+				}
+			}
+			else
+			{
+				ost++;
+			}
+		}
+	}
+}
+// funkcja zwracaj¹ca numer elementu pod wskazanymi wspó³rzêdnymi
+void licz::ktory_el(float x, float y, int& ost, Siatka S, bool& X)
+{
+	float nx, ny;
+	bool czy = true;	//czy kontynuowaæ poszukiwania elementu
+	X = false;		//zmienna awaryjna, bada prawid³owy przebieg pêtli
+	//zamiast zwyk³ego fora pêtla zaczyna poszukiwania od sprawdzenia ostatnio u¿ytego elementu.
+	//JEŒLI PUNKT NIE NALE¯Y DY ¯ADNEGO ELEMENTU, TO X = TRUE
 	while (czy)
 	{
 		ny = ost / S.kord_y.size();
@@ -67,9 +155,17 @@ float licz::temp(float x, float y, int& ost, std::vector<float> T, std::vector<P
 		}
 		else
 		{
-			if(ost == E.size())
+			if (ost == (S.kord_x.size()-1)*(S.kord_y.size()-1)-1)
 			{
 				ost = 0;
+				if (X)
+				{
+					czy = false;
+				}
+				else
+				{
+					X = true;
+				}
 			}
 			else
 			{
@@ -77,16 +173,4 @@ float licz::temp(float x, float y, int& ost, std::vector<float> T, std::vector<P
 			}
 		}
 	}
-	//Po powy¿szej pêtli wiadomo ju¿, w którym elemencie znajduje siê punkt
-	kx = (S.kord_x[nx + 1] - S.kord_x[nx]) * 0.5;
-	ky = (S.kord_y[ny + 1] - S.kord_y[ny]) * 0.5;		//wspó³czynniki proporcjonalnoœci
-	lokx = (x - S.kord_x[nx])/kx -1;
-	loky = (y - S.kord_y[ny])/ky -1;		//lokalne wspó³zêdne (-1;1)
-	n1 = nx + ny * S.kord_x.size();			//numer lewego dolnego wêz³a
-	w = T[n1] * (1 - lokx) * (1 - loky);
-	w += T[n1 + 1] * (1 + lokx) * (1 - loky);
-	w += T[n1 + S.kord_x.size()] * (1 - lokx) * (1 + loky);
-	w += T[n1 + S.kord_x.size() + 1] * (1 + lokx) * (1 + loky);
-	w *= 0.25;
-	return w;
 }
